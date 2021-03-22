6e489c533a7a572adea0e4f7359314ec2097e701
Igor M. Mouawad <igor@mouawad.com.br>
2021-03-22 17:39:50 -0300

    Adicionado a pagina Categorias

--- IonicAngularSpring/src/app/app.component.ts
+++ IonicAngularSpring/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Home', url: '/home', icon: 'home' }
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Categorias', url: '/categorias', icon: 'start' }
   ];
 
   constructor() {}
